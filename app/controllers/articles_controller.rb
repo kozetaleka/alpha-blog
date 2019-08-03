58f67cdc4f65a5bd3c8d31adbcf8621cfbde50c9
Study Ruby <kozileka@gmail.com>
2019-08-03 20:07:15 +0200

    Prepare for production

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  before_action :gjej_artikull, only: [:edit,:update, :show, :destroy]
+
   def index
     @articles=Article.all
   end
@@ -19,11 +21,9 @@ class ArticlesController < ApplicationController
   end
 
  def edit
-   @article=Article.find(params[:id])
  end
 
  def update
-   @article=Article.find(params[:id])
    if @article.update(article_params)
      flash[:notice]="Veprimi u krye me sukses!"
      redirect_to article_path(@article)
@@ -32,11 +32,9 @@ class ArticlesController < ApplicationController
    end
  end
   def show
-    @article=Article.find(params[:id])
   end
 
   def destroy
-    @article=Article.find(params[:id])
     @article.destroy
     flash[:notice]="Article was successfully deleted!"
     redirect_to articles_path
@@ -44,7 +42,10 @@ class ArticlesController < ApplicationController
   end
 
   private
-  def article_params
-    params.require(:article).permit(:title, :description)
-  end
+    def gjej_artikull
+      @article=Article.find(params[:id])
+    end
+    def article_params
+      params.require(:article).permit(:title, :description)
+    end
 end
